b32e712bb4013967c7baaf6c31f35c1ad1356417
Rasathivijayan <rasathi.v@enterprisetouch.net>
2019-12-23 17:23:06 +0530

    getReportGroupList, deleteReportGroup methods added

--- app/src/app/services/reportlistservice/reportlistservice.service.ts
+++ app/src/app/services/reportlistservice/reportlistservice.service.ts
@@ -10,46 +10,72 @@ import { map } from 'rxjs/operators';
 
 @Injectable()
 export class reportlistserviceService {
-     dm: ModelMethods
-     sysProps;
-     systemProperties: any;
-     updatename: any;
-     deleteName:any;
+    dm: ModelMethods
+    sysProps;
+    systemProperties: any;
+    updatename: any;
+    deleteName: any;
+    groupList: any; // groupList for table data in reportGroup page
+    selectedGroupId: number; // id of the group that was selected for any action
+
+    constructor(private bdms: NDataModelService, private http: HttpClient) {
+
+        this.dm = new ModelMethods(bdms);
+        this.sysProps = this.bdms;
+        this.systemProperties = this.sysProps.systemService.properties;
+        let url = this.systemProperties.modularUrl;
 
 
-     constructor(private bdms: NDataModelService, private http: HttpClient) {
+    }
+    getJson(): Observable<any> {
+        let url = this.systemProperties.modularUrl;
+        url += "grouplist"
+        console.log(url);
+        return this.http.get(url);
+    }
+    onSubmit(rname: any, rcode: any, rstatus: any, rid: any): Observable<any> {
 
-          this.dm = new ModelMethods(bdms);
-          this.sysProps = this.bdms;
-          this.systemProperties = this.sysProps.systemService.properties;
-          let url = this.systemProperties.modularUrl;
+        let url = this.systemProperties.modularUrl;
+        let reportName = "'" + rname + "'";
+        let reportCode = "'" + rcode + "'";
+        let reportStatus = "'" + rstatus + "'";
+        let reportId = "'" + rid + "'";
+        let data = { 'gname': reportName, 'gcode': reportCode, 'status': reportStatus, 'id': reportId };
+        url += "groupupdate "
+        let body = { 'gname': reportName, 'gcode': reportCode, 'status': reportStatus, 'id': reportId };
+        console.log(body);
+        return this.http.post(url, body);
+    }
+    onDeleteClick(dId: any): Observable<any> {
+        let url = this.systemProperties.modularUrl;
+        let deleteId = "'" + dId + "'";
+        url += "groupdelete"
+        let body = { 'id': deleteId };
+        return this.http.post(url, body);
+    }
 
+    getReportGroupList() {
+        this.http.get(`${this.systemProperties.modularUrl}groupList`).subscribe(groupList => {
+            if (groupList instanceof Array) {
+                this.groupList = groupList.map(item => {
+                    if (item.Active == 1) { item.Active = 'Active' }
+                    else { item.Active = 'Inactive' }
+                    return item;
+                });
+            } else {
+                this.groupList = []
+            }
+        }, error => {
+            console.log(error);
+        })
+    }
 
-     }
-     getJson(): Observable<any> {
-          let url = this.systemProperties.modularUrl;
-          url += "grouplist"
-          console.log(url);
-          return this.http.get(url);
-     }
-     onSubmit(rname: any, rcode: any, rstatus: any, rid: any): Observable<any> {
-
-          let url = this.systemProperties.modularUrl;
-          let reportName = "'" + rname + "'";
-          let reportCode = "'" + rcode + "'";
-          let reportStatus = "'" + rstatus + "'";
-          let reportId = "'" + rid + "'";
-          let data = { 'gname': reportName, 'gcode': reportCode, 'status': reportStatus, 'id': reportId };
-          url += "groupupdate "
-          let body = { 'gname': reportName, 'gcode': reportCode, 'status': reportStatus, 'id': reportId };
-          console.log(body);
-          return this.http.post(url, body);
-     }
-     onDeleteClick(dId: any): Observable<any> {
-          let url = this.systemProperties.modularUrl;
-          let deleteId = "'" + dId + "'";
-          url += "groupdelete"
-          let body = { 'id': deleteId };
-          return this.http.post(url, body);
-     }
+    deleteReportGroup() {
+        this.http.post(`${this.systemProperties.modularUrl}groupdelete`,
+            { 'id': this.selectedGroupId }).subscribe(deletedGroup => {
+                this.groupList = this.groupList.filter(v => v.id !== this.selectedGroupId)
+            }, error => {
+                console.log(error);
+            })
+    }
 }
