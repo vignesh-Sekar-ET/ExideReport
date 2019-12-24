426dc8efab7b72b2477a7addab2e3f01e6c39d9a
abinaya-as <abinaya.a@enterprisetouch.net>
2019-12-24 19:13:22 +0530

    usergroup mapping create

--- app/src/app/components/usergroupmappingcreateComponent/usergroupmappingcreate.component.ts
+++ app/src/app/components/usergroupmappingcreateComponent/usergroupmappingcreate.component.ts
@@ -1,5 +1,8 @@
 /*DEFAULT GENERATED TEMPLATE. DO NOT CHANGE SELECTOR TEMPLATE_URL AND CLASS NAME*/
-import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core'
+import {
+    Component, OnInit, ViewChild, ElementRef, Input,
+    Renderer2, ChangeDetectorRef, ViewContainerRef, ViewChildren, ChangeDetectionStrategy
+} from '@angular/core'
 import { ModelMethods } from '../../lib/model.methods';
 // import { BDataModelService } from '../service/bDataModel.service';
 import { NDataModelService } from 'neutrinos-seed-services';
@@ -8,6 +11,9 @@ import { NBaseComponent } from '../../../../../app/baseClasses/nBase.component';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { of } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { mappingserviceService } from '../../services/mappingservice/mappingservice.service';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+// import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 
 
@@ -31,155 +37,128 @@ export class usergroupmappingcreateComponent extends NBaseComponent implements O
     mm: ModelMethods;
     public form: FormGroup;
     public loadContent: boolean = false;
-    public multielect: "AbstractControl";
-    public singleselect: "AbstractControlsa";
+    mydata: any;
     singleArray: any = [];
-    @ViewChild('multiSelect', { static: false }) multiSelect: ElementRef;
-    public settings = {};
-    public singlesettings = {};
-    public selectedItems = [];
-    public data = [];
-    public bank = [];
-    public multiuserMapping = [];
-    public singleuserMapping = [];
-    public sample = [];
-    constructor(private bdms: NDataModelService, public element: ElementRef) {
+    @ViewChild('multiSelect', { static: false }) multiSelect;
+    public singleSelect = [];
+    public multiSelectArray = [];
+    public result: any;
+    public changingSample: any;
+    public usermapGroup = [];
+    public reportmapGroup = [];
+    public URarray = []; item; i = 0;
+    // foods = new BehaviorSubject(['Bacon', 'Letuce', 'Tomatoes']);
+    itemList: any = [];
+    selectedItems = [];
+    settings = {};
+    userGroupdatalist = [];
+    reportGroupdatalist: any = [];
+    dropdownSettingsUG = {};
+    dropdownSettingsRG = {};
+    public userGroup: any;
+    public reportGroup: any;
+    isSubmitted = false;
+    constructor(private bdms: NDataModelService, public renderer: Renderer2, public element: ElementRef, private mappingservice: mappingserviceService) {
         super();
         this.mm = new ModelMethods(bdms);
     }
 
     ngOnInit() {
+        this.selectedItems = [];
 
-        this.data = [
-            { name: 'Bank A (Switzerland)', id: 'A' },
-            { name: 'Bank B (Switzerland)', id: 'B' },
-            { name: 'Bank C (France)', id: 'C' },
-            { name: 'Bank D (France)', id: 'D' },
-            { name: 'Bank E (France)', id: 'E' },
-            { name: 'Bank F (Italy)', id: 'F' },
-            { name: 'Bank G (Italy)', id: 'G' },
-            { name: 'Bank H (Italy)', id: 'H' },
-            { name: 'Bank I (Italy)', id: 'I' },
-            { name: 'Bank J (Italy)', id: 'J' },
-            { name: 'Bank R (Germany)', id: 'R' }
-        ]
-
-        this.bank = [
-            { name: "sample", id: 1 },
-            { name: "sample1", id: 2 },
-            { name: "sample2", id: 3 },
-        ]
-        // singlese = true;
-
-        // setting and support i18n
-        this.settings = {
-            singleSelection: false,
-            idField: 'id',
-            textField: 'name',
-            enableCheckAll: true,
+        this.dropdownSettingsUG = {
+            text: "Select Skills",
             selectAllText: 'Select All',
-            unSelectAllText: 'Unselect All',
-            allowSearchFilter: true,
-            limitSelection: -1,
-            clearSearchFilter: true,
-            maxHeight: 197,
-            itemsShowLimit: 3,
-            searchPlaceholderText: 'search ',
-            noDataAvailablePlaceholderText: 'No data',
-            closeDropDownOnSelection: false,
-            showSelectedItemsAtTop: false,
-            defaultOpen: false
+            unSelectAllText: 'UnSelect All',
+            enableSearchFilter: true,
+            lazyLoading: true,
+            badgeShowLimit: 3,
+            labelKey: "cn",
+            primaryKey: "cn"
         };
-        // setting and support i18n
-        this.singlesettings = {
-            singleSelection: true,
-            idField: 'id',
-            textField: 'name',
-            enableCheckAll: true,
+        this.dropdownSettingsRG = {
+            text: "Select Skills",
             selectAllText: 'Select All',
-            unSelectAllText: 'Unselect All',
-            allowSearchFilter: true,
-            limitSelection: -1,
-            clearSearchFilter: true,
-            maxHeight: 197,
-            itemsShowLimit: 3,
-            searchPlaceholderText: 'search ',
-            noDataAvailablePlaceholderText: 'No data',
-            closeDropDownOnSelection: false,
-            showSelectedItemsAtTop: false,
-            defaultOpen: false
+            unSelectAllText: 'UnSelect All',
+            enableSearchFilter: true,
+            lazyLoading: true,
+            badgeShowLimit: 3,
+            labelKey: "Groupname",
+            primaryKey: "id"
         };
+
+        this.mappingservice.getReportGroup().subscribe((data) => {
+            this.reportGroupdatalist = data;
+            this.testExecutionFunction()
+        });
+
+        this.mappingservice.getUserGroup().subscribe((data) => {
+            this.userGroupdatalist = data["groups"];
+            this.testExecutionFunction()
+        });
         this.setForm();
-        // console.log(this.settings)
     }
 
     public setForm() {
         this.form = new FormGroup({
-            multiselect: new FormControl('', Validators.required),
-            singleselect: new FormControl('', Validators.required)
+            userGroup: new FormControl('', Validators.required),
+            reportGroup: new FormControl('', Validators.required)
         });
         this.loadContent = true;
     }
 
-
-    // save(event) {
-    //     let Mymap = new Map();
-    //     let mappingArray = [];
-    //     let singleArray = [];
-    //     let multiselectobj = event.multiselect.map(multitem => ({ id: multitem.id, name: multitem.name }))
-    //     let singleselectobj = event.singleselect.map(singleitem => singleitem.id);
-
-
-    //     mappingArray.push(multiselectobj)
-    //     singleArray.push(singleselectobj)
-    //     let obj1 = {};
-    //     // console.log(multiselectobj.assign(singleselectobj))
-    //     Mymap.set(...singleselectobj, ...mappingArray);
-    //     //       var obj = {multiselectobj};
-    //     // var pair = {singleselectobj};
-    //     // obj = {...obj, ...pair};
-    //     // console.log(obj)
-    //     for (let [key, value] of Mymap.entries()) {
-    //         console.log(multiselectobj)
-    //         // this.sample.push({ "value": { multiselectobj, key[0]})
-    //         // this.sample.push(...value) 
-    //         // Object.assign(this.sample, {key3: key});
-    //     }
-    //     console.log(this.sample)
-    //     const groupBy = (array, key) => {
-    //         console.log(array)
-    //         return array.reduce((result, currentValue) => {
-    //             (result[currentValue.key] = result[currentValue.key] || []).push(
-    //                 currentValue
-    //             );
-    //             console.log(result);
-    //             return result;
-    //         }, {});
-    //     };
-
-    //     const personGroupedByColor = groupBy(this.sample, key);
-
-    //     // console.log(this.sample)
-
-    // }
-
+    testExecutionFunction() {
+        this.userGroupdatalist;
+        this.reportGroupdatalist;
+    }
+    get getuserGroup() { return this.form.get('userGroup'); }
+    get getReportGroup() { return this.form.get('reportGroup'); }
     save(event) {
+        this.isSubmitted = true;
+        if (this.form.invalid) {
+            return false;
+        }
+        else {
+            this.uniqueArrayUserGroup(event.userGroup)
+            this.uniqueArrayReportGroup(event.reportGroup)
+            let Umap = this.usermapGroup.map(item => ({ Usergroup: item.cn, reporGroupmap: [...new Set(event.reportGroup)] }))
+            let Rmap = this.reportmapGroup.map(item => ({ ReportGroup: item.Groupname, userGroupmap: [...new Set(event.userGroup)] }))
+            console.log("ReportGroup", Rmap, 'Usergroup', Umap)
+            console.log(JSON.stringify(Rmap) )
+            let umapping = Umap.map((userGroup) => {
+                // let result = {
+                //     key:userGroup.Usergroup
+                // }
+                console.log(userGroup)
+            })
+        }
 
-        let destructure = [...event.multiselect]
-        Object.assign(destructure, { key: event.singleselect[0].id })
-        console.log(destructure)
-        let obj = destructure;
-        console.log(JSON.stringify(obj))
-        this.sample.push(obj)
-        const personGroupedByColor = this.groupBy(this.sample, "key");
-        console.log("pC", JSON.stringify(personGroupedByColor[event.singleselect[0].id]))
-        personGroupedByColor[event.singleselect[0].id] = destructure;
-        
-        // this.finalArray(personGroupedByColor["1"][0], 'id')
-        console.log("pC", JSON.stringify(personGroupedByColor))
 
     }
 
+    uniqueArrayUserGroup(array) {
+      
+        this.usermapGroup = [];
+        for (let i = 0; i < array.length; i++) {
+            if (this.usermapGroup.indexOf(array[i]["cn"]) === -1) {
+                this.usermapGroup.push(array[i])
+            }
+        }
+    }
+    uniqueArrayReportGroup(array) {
+        this.reportmapGroup = [];
+        for (let i = 0; i < array.length; i++) {
+            if (this.reportmapGroup.indexOf(array[i]["id"]) === -1) {
+                this.reportmapGroup.push(array[i])
+            }
+        }
+    }
+    removeDuplicates(array, key) {
+        console.log(array, key)
+        let lookUp = new Set();
+        console.log(lookUp)
+        return array.filter(item => !lookUp.has(item[key]) && lookUp.add(item[key]))
+    }
     groupBy = (array, key) => {
         // console.log(array, key)
         return array.reduce((result, currentValue) => {
@@ -192,32 +171,21 @@ export class usergroupmappingcreateComponent extends NBaseComponent implements O
 
     };
 
-    finalArray(array, key) {
 
-        let lookup = new Set()
-        console.log(lookup)
-        return array.filter(obj => !lookup.has(obj[key]) && lookup.add(obj[key]))
+    clear() {
+        this.isSubmitted = false;
+        this.form.reset();
+        this.form.markAsDirty()
+        this.form.markAsTouched()
     }
-
-
-    public onFilterChange(item: any) {
-        // console.log(item);
+    onItemSelect(item: any) {
     }
-    public onDropDownClose(item: any) {
-        // console.log(item);
+    OnItemDeSelect(item: any) {
+        this.isSubmitted = false;
     }
-
-    public onItemSelect(item: any) {
-        // console.log(item);
+    onSelectAll(items: any) {
     }
-    public onDeSelect(item: any) {
-        // console.log(item);
-    }
-
-    public onSelectAll(items: any) {
-        // console.log(items);
-    }
-    public onDeSelectAll(items: any) {
-        // console.log(items);
+    onDeSelectAll(items: any) {
+        this.clear();
     }
 }
