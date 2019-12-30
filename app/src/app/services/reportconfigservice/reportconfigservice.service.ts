0ae66e6ef9d3a18441af307336a3c0dee8f8fcfb
Rasathivijayan <rasathi.v@enterprisetouch.net>
2019-12-30 16:16:38 +0530

    cleared conflit issues

--- app/src/app/services/reportconfigservice/reportconfigservice.service.ts
+++ app/src/app/services/reportconfigservice/reportconfigservice.service.ts
@@ -5,56 +5,71 @@ import { ModelMethods } from 'app/lib/model.methods';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { NSnackbarService } from 'neutrinos-seed-services';
+
 @Injectable()
 export class reportconfigserviceService {
     dm: ModelMethods
     sysProps;
     systemProperties: any;
     dropdownlist: any;
-constructor(private bdms: NDataModelService, private http: HttpClient) {
+    reportid: number;
+    reportconfiglist: any;
+    changecomp: any;
+    updatename: any;
+    constructor(private bdms: NDataModelService, private http: HttpClient) {
 
         this.dm = new ModelMethods(bdms);
         this.sysProps = this.bdms;
         this.systemProperties = this.sysProps.systemService.properties;
         let url = this.systemProperties.modularUrl;
-
-
     }
-    getReportConfigList() {
+    configcreatelist() {
         let url = this.systemProperties.modularUrl;
-        url += "reportconfig"
-        console.log(url);
+        url += "configcreatelist"
+console.log("uuuuu"+ url)
         return this.http.get(url);
     }
-    onSubmit(rname: any, scheduledtype: any, scheduleddate: any, time: any, networklocation: any, starttime: any, endtime: any, email: any, downloable: any, selectedstatuss: any): Observable<any> {
-
+    onSubmit(rname: any, networklocation: any, starttime: any, endtime: any, email: any, downloable: any, selectedstatuss: any): Observable<any> {
         let url = this.systemProperties.modularUrl;
         let reportName = "'" + rname + "'";
-        let schedtype = "'" + scheduledtype + "'";
-        let schedddate = "'" + scheduleddate + "'";
-        let timee = "'" + time + "'";
         let networklocationn = "'" + networklocation + "'";
         let starttimee = "'" + starttime + "'";
         let endtimee = "'" + endtime + "'";
-        let downloablee= "'" + downloable + "'";
+        let downloablee = "'" + downloable + "'";
         let selectedstatus = "'" + selectedstatuss + "'";
-        // let data = { 'gname': reportName, 'gcode': reportCode,'status':reportStatus,'id':reportId};
-        url += "groupupdate "
-        let body = { 'gname': reportName, 'gcode': schedtype, 'status': schedddate, 'id': timee, 'network': networklocationn, 'starttime': starttimee, 'endtime': endtimee, 'downloable': downloablee, 'statuss': selectedstatus };
-        // console.log(body);
+        let emailsubscription = "'" + email + "'";
+        url += "createreportconfig "
+        let body = { 'reports_id': reportName, 'network': networklocationn, 'starttime': starttimee, 'endtime': endtimee, 'isdownloadable': downloablee, 'status': selectedstatus, 'issubscribe': emailsubscription };
         return this.http.post(url, body);
     }
+    getReportConfigList() {
+        this.http.get(`${this.systemProperties.modularUrl}reportconfiglist`).subscribe(reportconfiglist => {
+            console.log("url" + JSON.stringify(reportconfiglist))
 
-
-
-
-    
-
-
-
-
-
-
+            if (reportconfiglist instanceof Array) {
+                this.reportconfiglist = reportconfiglist.map(item => {
+                    // item.isdownloadable == 1 ? "Yes" : "No";
+                    if (item.isdownloadable == 1) { item.isdownloadable = 'Yes' }
+                    else { item.isdownloadable = 'No' }
+                    if (item.issubscribe == 1) { item.issubscribe = 'Yes' }
+                    else { item.issubscribe = 'No' }
+                    if (item.status == 1) { item.status = 'Yes' }
+                    else { item.status = 'No' }
+                    let timestart = new Date(item.starttime);
+                    let setStartTime = timestart.getUTCHours() + ':' + timestart.getUTCMinutes();
+                    item.starttime = setStartTime;
+                    let timeend = new Date(item.endtime);
+                    let setEndtime = timeend.getUTCHours() + ':' + timeend.getUTCMinutes();
+                    item.endtime = setEndtime;
+                    return item;
+                });
+            } else {
+                this.reportconfiglist = []
+            }
+        }, error => {
+            console.log(error);
+        })
+    }
 
 }
